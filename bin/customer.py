dc80238a849fb7192ff1e813d3026244e24ca156
Seleman Hemed <shemed.blackops@gmail.com>
2020-12-01 15:15:59 +0300

    Added customer class

--- bin/customer.py
+++ bin/customer.py
@@ -1,3 +1,28 @@
 class Customer:
-    def __str__(self):
-        print("Printing customer")
+    def __init__(self, account_no):
+        self.account_no = account_no
+        self.balance = 0
+
+    def get_fullname(self):
+        return f"{self.firstname} {self.surname}"
+
+    def deposit(self, amount):
+        self.balance += amount
+        return True
+
+    def get_balance(self):
+        return self.balance
+
+    def withdraw(self, amount):
+        if self.balance < amount:
+            raise ValueError("Insufficient balance... Please deposit")
+        else:
+            self.balance -= amount
+
+    def print_summary(self):
+        summary = f"""
+        Account Name: {self.get_fullname()}
+        Account No. :  {self.account_no}
+        Balance     : {self.get_balance()}
+        """
+        return summary
