652eb0e148f8788aebe9a0f074e6a6d80bbb33b8
Seleman Hemed <shemed.blackops@gmail.com>
2020-12-03 23:49:18 +0300

    Added account generation to customer object

--- bin/customer.py
+++ bin/customer.py
@@ -6,6 +6,7 @@ class Customer(Account):
         super().__init__()
         self.details = details
         self.fullname = self.get_fullname()
+        self.account_no = Account().generate_account_no()
 
     def get_fullname(self):
         return f"{self.details.get('first_name')} {self.details.get('last_name')}"
