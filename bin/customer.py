9f39abb2e1d3c6d512b6c2ea258ec5bb47377e31
Seleman Hemed <shemed.blackops@gmail.com>
2020-12-05 16:37:21 +0300

    print account summary after registration

--- bin/customer.py
+++ bin/customer.py
@@ -8,11 +8,21 @@ class Customer(Account):
     def __init__(self):
         super().__init__()
         self.details = {}
-        # self.fullname = self.get_fullname()
+        self.fullname = self.get_fullname()
         self.account_no = Account().generate_account_no()
 
     def get_fullname(self):
         return f"{self.details.get('first_name')} {self.details.get('last_name')}"
+
+    def initial_account_details(self):
+        output = f"""
+               ***************************************
+               Name : {self.get_fullname()}
+               Account No: {self.account_no}
+               Account Balance: {self.account_balance} 
+               ****************************************
+               """
+        return dedent(output)
 
     def account_summary(self, account_no):
         db = Database().connect()
